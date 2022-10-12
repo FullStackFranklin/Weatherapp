7ab4a8b0802e2d42dffe7247abfbe25c832acd57
MimiFullstackFranklin <107124830+FullStackFranklin@users.noreply.github.com>
2022-10-11 23:41:34 -0400

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,16 @@
+document.getElementById('submit').addEventListener("click", function () {
+    const sCityName = document.getElementById("search").value;
+    validateUserInput(sCityName);
+
+});
+
+//Ensure user input has valid city name and length
+function validateUserInput(sCityName) {
+    if (sCityName === null || sCityName.length < 4) 
+        return; 
+    storeUserHistory(sCityName);
+}
+//Stores user history in list item format 
+function storeUserHistory(sCityName){
+//console.log(sCityName);
+}
