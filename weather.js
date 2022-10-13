3ed3ad0f76f88124c5d2412842fb90f5522af7a0
MimiFullstackFranklin <107124830+FullStackFranklin@users.noreply.github.com>
2022-10-13 06:43:20 -0400

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,7 +1,8 @@
 let aCityList = [];//store array search list
 const ulCityList = document.getElementById('historyList');//unordered list city data
-
-
+const nMaxHistoryStored=5; //sets the number of stored history items to only 5
+const sApiKey='2679b512ffea0aa250ddb289f6418fe6'; //string is an API key being used to fetch data
+let sUrlApi; //url api data will be in string format
 
 
 
@@ -14,11 +15,21 @@ document.getElementById('submit').addEventListener("click", function () {
 //Ensure user input has valid city name and length
 function validateUserInput(sCityName) {
     if (sCityName === null || sCityName.length < 4) 
-        return; 
-    storeUserHistory(sCityName);
+        return; //will stop the function based on above params
+    readCityLocation(sCityName);
+    
 }
 //Stores user history in list item format 
 function storeUserHistory(sCityName){
 //console.log(sCityName);
+    if(aCityList.length >= nMaxHistoryStored) //validate history length
+        removeOldHistory();
+        aCityList.push(sCityName); //add to list array
+        setLocalStorage();
+        cleanList();
+        loadHistory();
+
+        
+
 }
 
